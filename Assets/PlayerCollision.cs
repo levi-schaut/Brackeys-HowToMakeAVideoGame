f7f776b5e868ac90e98b1217c39fb74eb88e5c24
Levi Schaut <schaut.levi@gmail.com>
2023-05-18 15:08:07 -0500

    Add PlayerCollision.cs
    Adds the "PlayerCollision" script to detect when the player has collided with an obstacle.

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movementScript;
+    private void OnCollisionEnter(Collision collisionInfo) 
+    {
+        if (collisionInfo.gameObject.tag == "Obstacle") {
+            movementScript.enabled = false;
+        }
+    }
+}
