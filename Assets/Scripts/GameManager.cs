264296bff863cccb0dd566db6df9773e7523ff3b
Levi Schaut <schaut.levi@gmail.com>
2023-05-18 18:20:13 -0500

    Update GameManager.cs
    Updates Game Manager to ignore repeat calls to its "GameOver()" method if the game has already ended.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    bool gameHasEnded = false;
+
     /* This custom method represents a game over.  It can be called by any
      * other GameObject that detects a loss condition, and it initiates a
      * game restart. */
     public void GameOver() {
-        Debug.Log("GAME OVER");
+        if (!gameHasEnded) {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+        }
     }
 }
